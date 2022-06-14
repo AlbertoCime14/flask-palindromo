64d9684ea1d5317bbca94524f328cf0b16221d48
Alberto Cime <alberto_cime@epam.com>
2022-06-14 14:29:38 -0500

    modificacion de la logica del ejercicio

--- source/app.py
+++ source/app.py
@@ -1,11 +1,40 @@
-import imp
-from flask import Flask
+from flask import Flask, request, jsonify
 import os
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'This is my first API call!'
+@app.route('/', methods=['GET'])
+def palindrome():
+    # Check if an ID was provided as part of the URL.
+    # If ID is provided, assign it to a variable.
+    # If no ID is provided, display an error in the browser.
+    def isPalindrome(s):
+        return s == s[::-1]
+    
+    if 'string' in request.args:
+        strings_args= request.args['string']
+        if strings_args.isdigit() :
+            return "please, insert a string"
+        else:
+            palindrome =isPalindrome(strings_args)
+            if palindrome:
+                Dict = {"palindrome" : "True"}
+                return Dict
+            else:
+                Dict = {"palindrome" : "False"}
+                return Dict
+    else:
+        return "Error: No id field provided. Please specify a string"
+
+    # Create an empty list for our results
+    
+
+    # Loop through the data and match results that fit the requested ID.
+    # IDs are unique, but other fields might return many results
+   
+
+    # Use the jsonify function from Flask to convert our list of
+    # Python dictionaries to the JSON format.
+    return jsonify(strings_args)
 
 if __name__=='__main__':
     app.run(port=int(os.environ.get("PORT", 8080)),host='0.0.0.0',debug=True)#port add
