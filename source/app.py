cded14d79ff80b4bc6a731369046b24e772e18d9
Alberto Cime <alberto_cime@epam.com>
2022-06-14 13:24:14 -0500

    se cambio el puerto de escucha de la aplicación2

--- source/app.py
+++ source/app.py
@@ -1,4 +1,6 @@
+import imp
 from flask import Flask
+import os
 app = Flask(__name__)
 
 @app.route('/')
@@ -6,6 +8,6 @@ def hello_world():
     return 'This is my first API call!'
 
 if __name__=='__main__':
-    app.run(host="0.0.0.0", port=4000, debug=True)
+    app.run(port=int(os.environ.get("PORT", 8080)),host='0.0.0.0',debug=True)
 
 
